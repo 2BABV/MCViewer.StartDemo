723c2903a216a063edd49929975bd19e04532fd1
Joost Roza <joost@ttt.nl>
2021-01-25 16:14:40 +0100

    #1 (node app) Changed the request to work nicely with the upcoming api change

--- MCViewer.JS.Demo/app.js
+++ MCViewer.JS.Demo/app.js
@@ -4,39 +4,39 @@ const axios = require('axios')
 const open = require('open');
 
 
-var postObj = {
-    hookUrl: 'https://www.2ba.nl',
-    languageCode: 'NL',
-    json: JSON.stringify({
-        "ClassId": "MC000043",
-        "Features": [
-            {
-                "FeatureId": "EF010527",
-                "Portcode": 2,
-                "NumericValue": 89,
-                "LogicalValue": null,
-                "RangeLowerValue": null,
-                "RangeUpperValue": null,
-                "AlphaNumericValue": null
-            }
-        ]
-    })
-};
+var postObj = ({
+	hookUrl: 'https://www.2ba.nl',
+	languageCode: 'NL',
+	content: {
+		classId: "MC000043",
+		features: [
+			{
+				featureId: "EF010527",
+				portcode: 2,
+				numericValue: 89,
+				logicalValue: null,
+				rangeLowerValue: null,
+				rangeUpperValue: null,
+				alphaNumericValue: null
+			}
+		]
+	}
+});
 
 console.log(postObj);
-
+let baseUrl = "https://mc.2ba.nl";
 axios
-    .post('https://mc.2ba.nl/ApiService/GetViewCodeFromInputFromBody', postObj, {
-        headers: {
-            'Content-Type': 'application/json',
-        }
-    })
-    .then((res) => {
-        console.log(`statusCode: ${res.statusCode}`)
-        console.log(res.data)
+	.post(baseUrl + '/ApiService/GetViewCodeFromInputFromBody', postObj, {
+		headers: {
+			'Content-Type': 'application/json',
+		}
+	})
+	.then((res) => {
+		console.log(`statusCode: ${res.status} - ${res.statusText}`)
+		console.log(res.data)
 
-        open("https://mc.2ba.nl/NL/View/" + res.data.ViewCode);
-    })
-    .catch((error) => {
-        console.error(error)
-    });
+		open(baseUrl + "/NL/View/" + res.data.ViewCode);
+	})
+	.catch((error) => {
+		console.error(error)
+	});
